defed4f585140be413a6cab6d9af34b162417c21
Camellia <camellia.sialiss@gmail.com>
2024-05-01 16:39:38 +0300

    Updates

---
+++ assets/ContactsView-CF-mNJfB.js
@@ -0,0 +1,1 @@
+import{d as a,a as t,b as s,F as o,e,g as n,o as c,T as r}from"./index-DL3hwRCc.js";const l=e("main",null,[e("div",{class:"about"},[e("h1",null,[n(" Contact us at "),e("a",{href:"tel:+7800553535"},"8 (800) 555 35-35")])])],-1),i=a({__name:"ContactsView",setup(_){return(d,u)=>(c(),t(o,null,[s(r),l],64))}});export{i as default};

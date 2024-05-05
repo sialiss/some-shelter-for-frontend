3e7e261bd218fd847a3275b73568434a36d3ec0a
och-och <och.hco.och.hco@gmail.com>
2024-05-05 18:40:50 +0300

    help us page

--- src/views/HelpView.vue
+++ src/views/HelpView.vue
@@ -5,10 +5,156 @@ import TheHeader from "../components/TheHeader.vue"
 <template>
 	<TheHeader />
 	<main>
-		<div class="about">
-			<h1>HELP US</h1>
+		<div class="first-screen | stack just-center">
+			<div class="fix-width pad | flex equal">
+				<div class="stack gap-xl just-center pad-m">
+					<h2>Как вы можете помочь<br />нашему приюту</h2>
+					<div class="help-items">
+						<div class="help-item">
+							<img
+								src="/src/assets/icons/icon-pet-food.svg"
+								alt=""
+								class="icon"
+							/>
+							<div class="name">Корм</div>
+						</div>
+						<div class="help-item">
+							<img
+								src="/src/assets/icons/icon-transportation.svg"
+								alt=""
+								class="icon"
+							/>
+							<div class="name">Транспорт</div>
+						</div>
+						<div class="help-item">
+							<img
+								src="/src/assets/icons/icon-toys.svg"
+								alt=""
+								class="icon"
+							/>
+							<div class="name">Игрушки</div>
+						</div>
+						<div class="help-item">
+							<img
+								src="/src/assets/icons/icon-bowls-and-cups.svg"
+								alt=""
+								class="icon"
+							/>
+							<div class="name">Миски</div>
+						</div>
+						<div class="help-item">
+							<img
+								src="/src/assets/icons/icon-shampoos.svg"
+								alt=""
+								class="icon"
+							/>
+							<div class="name">Шампунь</div>
+						</div>
+						<div class="help-item">
+							<img
+								src="/src/assets/icons/icon-vitamins.svg"
+								alt=""
+								class="icon"
+							/>
+							<div class="name">Витаминки</div>
+						</div>
+						<div class="help-item">
+							<img
+								src="/src/assets/icons/icon-medicines.svg"
+								alt=""
+								class="icon"
+							/>
+							<div class="name">Лекартсва</div>
+						</div>
+						<div class="help-item">
+							<img
+								src="/src/assets/icons/icon-collars-leashes.svg"
+								alt=""
+								class="icon"
+							/>
+							<div class="name">Ошейники</div>
+						</div>
+						<div class="help-item">
+							<img
+								src="/src/assets/icons/icon-sleeping-area.svg"
+								alt=""
+								class="icon"
+							/>
+							<div class="name">Кроваточки</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="second-screen">
+			<div class="fix-width pad-xl | flex wrap align-center just-center">
+				<img
+					src="/src/assets/images/donation-dog.png"
+					alt="грустная собачка хочет кушать ваши деньги"
+				/>
+				<div class="stack">
+					<h2>А ещё мы принимаем донатики</h2>
+					<div class="flex wrap align-center just-center">
+						<p>СуперБанк&reg;</p>
+						<div class="card-number | flex">
+							<img
+								src="/src/assets/icons/credit-card.svg"
+								alt="банковская карточка"
+							/>
+							<span
+								>8496&nbsp;4739&nbsp;4342&nbsp;8679&nbsp;7861</span
+							>
+						</div>
+					</div>
+					<div class="small-text">
+						Легальные штуки - это важная часть нашей повседневной
+						жизни. Они обеспечивают нам безопасность, защиту прав и
+						справедливость. Будь то законы, контракты или лицензии,
+						легальные штуки помогают нам поддерживать порядок и
+						стабильность в обществе. Без них было бы сложно
+						обеспечить права и интересы каждого человека. Поэтому
+						важно быть в курсе всех легальных штук и соблюдать их,
+						чтобы жить в гармонии и безопасности.
+					</div>
+				</div>
+			</div>
 		</div>
 	</main>
 </template>
 
-<style></style>
+<style>
+.first-screen {
+	background-color: var(--color-light-xl);
+	padding-block: 5rem;
+}
+
+h2 {
+	text-align: center;
+}
+
+.help-items {
+	display: flex;
+	flex-wrap: wrap;
+	gap: 7rem;
+	row-gap: var(--pad-xl);
+	justify-content: center;
+}
+
+.help-item {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+
+.card-number {
+	padding: var(--pad-m);
+	background: var(--color-primary);
+	border-radius: 30px;
+}
+
+.small-text {
+	max-width: 500px;
+	font-size: small;
+	color: var(--color-dark-m);
+}
+</style>

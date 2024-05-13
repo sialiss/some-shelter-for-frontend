1611f36c704de6ceb2095e7adb0463d60d3332f4
Camellia <56206613+sialiss@users.noreply.github.com>
2024-05-13 15:50:01 +0300

    add some text to contacts page

--- src/views/ContactsView.vue
+++ src/views/ContactsView.vue
@@ -1,16 +1,64 @@
 <script setup lang="ts">
 import TheHeader from "../components/TheHeader.vue"
+import { ref, onBeforeMount, onUnmounted } from "vue"
+
+const isHeaderDark = ref(true)
+
+function scrollHandler() {
+	isHeaderDark.value = window.scrollY == 0
+}
+
+onBeforeMount(() => document.addEventListener("scroll", scrollHandler))
+onUnmounted(() => document.removeEventListener("scroll", scrollHandler))
 </script>
 
 <template>
-	<TheHeader />
+	<TheHeader fixed :transparent="isHeaderDark" :dark="isHeaderDark" />
 	<main>
-		<div class="about">
-			<h1>
-				Contact us at <a href="tel:+7800553535">8 (800) 555 35-35</a>
-			</h1>
+		<div class="contacts screen | stack just-center">
+			<div class="fix-width pad | flex equal">
+				<div class="stack just-center">
+					<h2>Для вопросов и предложений</h2>
+					<p>
+						Lorem ipsum dolor sit amet consectetur, adipisicing
+						elit. Dolorum consequatur, corporis ratione natus facere
+						hic? Dolorum nisi quis, harum sint placeat praesentium
+						veritatis esse, nobis soluta voluptatibus, aspernatur
+						laboriosam suscipit.
+					</p>
+				</div>
+				<div>
+					<h2>Мы ждём Вашего визита</h2>
+					<p>
+						Lorem ipsum dolor sit amet consectetur, adipisicing
+						elit. Dolorum consequatur, corporis ratione natus facere
+						hic? Dolorum nisi quis, harum sint placeat praesentium
+						veritatis esse, nobis soluta voluptatibus, aspernatur
+						laboriosam suscipit.
+					</p>
+				</div>
+			</div>
 		</div>
 	</main>
 </template>
 
-<style></style>
+<style>
+.contacts {
+	background-image: linear-gradient(-45deg,
+			var(--start-screen-gradient-background),
+			var(--start-screen-gradient-background-2));
+}
+
+.contacts p {
+	color: var(--color-dark-s);
+}
+
+.contacts h2 {
+	color: var(--color-light-xl);
+}
+
+a {
+	color: var(--color-dark-l);
+	text-decoration-line: none;
+}
+</style>
